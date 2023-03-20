8ffbba9c6ee1e245e1ee43ceda2d9cec6972af2f
meeky <98189596+mmsaki@users.noreply.github.com>
2023-03-19 19:31:45 -0500

    feat: can now connect wallet

--- src/components/assets/PlayButton.tsx
+++ src/components/assets/PlayButton.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import { ConnectKitButton } from 'connectkit';
+
 const PlayButton = () => {
-  return <button className='play-button'>Play Now</button>;
+  return (
+    <>
+      <ConnectKitButton.Custom>
+        {({
+          isConnected,
+          isConnecting,
+          show,
+          hide,
+          address,
+          ensName,
+          chain,
+          truncatedAddress,
+        }) => {
+          return (
+            <button onClick={show} className='play-button'>
+              {isConnected ? 'Log Out' : 'Play Now'}
+            </button>
+          );
+        }}
+      </ConnectKitButton.Custom>
+    </>
+  );
 };
 
 export default PlayButton;
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { WagmiConfig, createClient } from 'wagmi';
+import { goerli } from 'wagmi/chains';
+import { ConnectKitProvider, getDefaultClient } from 'connectkit';
+import { ChakraProvider } from '@chakra-ui/react';
+
+const client = createClient(
+  getDefaultClient({
+    appName: 'Jackpot Lottery on Goerli',
+    infuraId: process.env.REACT_APP_INFURA_ID,
+    alchemyId: process.env.REACT_APP_ALCHEMY_ID,
+    chains: [goerli],
+  })
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <WagmiConfig client={client}>
+        <ConnectKitProvider theme='auto'>
+          <App />
+        </ConnectKitProvider>
+      </WagmiConfig>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
